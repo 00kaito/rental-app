a8d404c19aed1a286b46fd0747f42831db45af55
00kaito <bednarz@programmer.net>
2020-12-30 11:40:45 +0100

    Adding new apartment - add missing rooms to Apartment

--- src/main/java/org/pbednarz/rentalapp/domain/apartment/Apartment.java
+++ src/main/java/org/pbednarz/rentalapp/domain/apartment/Apartment.java
@@ -1,13 +1,17 @@
 package org.pbednarz.rentalapp.domain.apartment;
 
+import java.util.List;
+
 public class Apartment {
     private final String ownerId;
     private final Address address;
+    private List<Room> rooms;
     private String description;
 
-     Apartment(String ownerId, Address address, String description) {
+     Apartment(String ownerId, Address address, List<Room> rooms, String description) {
         this.ownerId = ownerId;
         this.address = address;
-        this.description = description;
+         this.rooms = rooms;
+         this.description = description;
     }
 }
